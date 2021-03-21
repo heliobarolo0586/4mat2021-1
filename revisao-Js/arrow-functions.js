481bb16b118062412b5aa369b2ee179f55266e9f
heliobarolo0586 <heliobarolo0586@gmail.com>
2021-03-21 20:07:25 +0000

    Aula 04 - 4320.

--- revisao-Js/arrow-functions.js
+++ revisao-Js/arrow-functions.js
@@ -27,4 +27,50 @@ console.log(potencia(2, 5))
 //No formato arrow function, quando há mais de um parâmetro
 //parânteses em volta deles retornam
 let potencia2 = (b, e) => b ** e
-console.log(potencia2(2,5))
+console.log(potencia2(2, 5))
+
+//Funçao sem parâmetros
+function megasena() {
+    //esta função retorna um número aleatório entre 1 e 60
+    //Math.random()->retorna um número aleatório entre 0 (inclusive) e 1 (exclusive)
+    //*60 -> Ajusta a faixa de valores para entre 0 e 59
+    //+1 ->Ajusta a faixa de valores para entre 1 e 60
+    //Math.floor() -> retira a parte fracionaria(casas decimais) do resultado
+    return Math.floor(Math.random() * 60 + 1)
+}
+
+//6 números de um jogo da megasena
+console.log(megasena(), megasena(), megasena(), megasena(), megasena(), megasena())
+
+
+//Em arrow functions sem parâmetros, é necesário deixar um par vazio de parênteses
+//Para marcar o lugar dos parênteses
+const megasena2 = () => Math.floor(Math.random() * 60 + 1)
+
+//6 números de um jogo da megasena
+console.log(megasena2(), megasena2(), megasena2(), megasena2(), megasena2(), megasena2())
+
+
+// Função com mais de uma linha de código no corpo
+function somaTudo(...nums){
+    let soma = 0
+    for (n  of nums) soma += n
+    return soma
+}
+console.log(somaTudo(1,2,3,4,5,6,7,8,9))
+
+//Em arrow functions com mais de uma linha no corpo, as chaves retornam e 
+//também é necessário voltar a usar a palavra return para retornar um valor  
+
+const somaTudo2 = (...nums)=>{
+    let soma = 0
+    //O parâmetro de resto, dentro de uma função, é interpretado como um vetor 
+    for (n  of nums) soma += n
+    return soma
+} 
+
+console.log(somaTudo2(1,2,3,4,5,6,7,8,9))
+
+
+
+
---
+++ revisao-Js/includes/func1.mjs
@@ -0,0 +1,13 @@
+function somaVet(vet){
+    let soma = 0
+    for(let n of vet)soma += n
+    return soma     
+}
+
+function quadradoVet(vet){
+    //Cria  um novo vetor cujo os elementos equivalem
+    //aos elementos do vetor original elevados ao quadrado
+    let res = []
+    for(let n of vet)res.push(n**2)
+    return res
+}
